f82c0706f4df5a8bc22300fc429bc3d00d1570b1
Garcia Nieto <alicia.garcianieto@nrcan-rncan.gc.ca>
2024-12-30 10:31:38 -0800

    parent graph class and graph subclasses bar ebones completed

--- NGraphsWidget.py
+++ NGraphsWidget.py
@@ -18,122 +18,118 @@ import pyqtgraph as pg
 import numpy as np
 
 
-
-"""
-Handles the computation for the graph based on sliders.
-"""
-class GraphCalculator(QObject):
-
-    # Declare the signal at the class level
-    data_changed = pyqtSignal()
-    
-    def __init__(self, sliders_widget):
-        super().__init__()  # Initialize QObject
-        self.sliders_widget = sliders_widget
-
-    def compute_graph_data(self):
-        """
-        Computes the x and y values for the graph.
-        Returns: (x, y) tuple
-        """
-        suma = sum(slider.get_value() for slider in self.sliders_widget.list_of_sliders)
-        x = np.linspace(-100, 100, 1000)
-        a = 1  # Quadratic coefficient
-        y = a * (x - suma) ** 2  # Quadratic function
-        return x, y
-    
-    def update_graph(self):
-        """
-        Emits a signal when graph data changes.
-        """
-        self.data_changed.emit()
-
-"""
-Displays the graph based on computed data from GraphCalculator.
-"""
-class GraphWidget(pg.PlotWidget):
-    
-    def __init__(self, calculator):
+class ParentGraph(pg.PlotWidget):
+    def __init__(self):
         super().__init__()
-        self.calculator = calculator
-        self.setTitle("Quadratic Equation: y = ax^2 + bx + c")
-        self.setLabel('left', 'y')
-        self.setLabel('bottom', 'x')
 
-        # Connect calculator's data_changed signal to refresh_graph
-        self.calculator.data_changed.connect(self.refresh_graph)
+        #MM
+        #figure out a better set of initialization values
+        freq = np.array([1, 10, 100, 1000, 10000])  
+        Z_real = np.array([100, 80, 60, 40, 20]) 
+        Z_imag = np.array([-50, -40, -30, -20, -10]) 
+
+        self.setTitle("Parent Graph")
+        self.showGrid(x=True, y=True)
         
         # Initial graph display
-        self.refresh_graph()
+        self.refresh_graph(freq,Z_real,Z_imag)
 
-    def refresh_graph(self):
-        """
-        Updates the graph display with the latest computed data.
-        """
-        x, y = self.calculator.compute_graph_data()
+    def refresh_graph(self,freq,x ,y):
+        #MM
+        #should I jsut write pass in here?
         self.clear()  # Clear previous plot
         self.plot(x, y)
 
 
+
+
+
+class PhaseGraph(ParentGraph):
+    def __init__(self): 
+        super().__init__()
+
+        self.setTitle("Phase")
+        self.setLabel('bottom', "Frequency [Hz]")
+        self.setLabel('left', "Phase [degrees]")
+        
+        # Initial graph display
+        # Initial graph display
+
+    def refresh_graph(self,freq,Z_real,Z_imag):
+        
+        phase = np.arctan2(Z_imag, Z_real) * 180 / np.pi  # Phase of Z (in degrees)
+        self.clear()  # Clear previous plot
+        self.plot(freq, phase)
+
+
+class BodeGraph(ParentGraph):
+    def __init__(self):
+        super().__init__()
+
+        self.setTitle("Bode Graph")
+        self.setLabel('bottom', "Freq [Hz]")
+        self.setLabel('left', "Total Impedance [Ohms]")
+        #self.setAspectLocked(True)  # Lock aspect ratio to 1:1
+
+    def refresh_graph(self,freq,Z_real,Z_imag):
+        
+        magnitude = np.sqrt(Z_real**2 + Z_imag**2)
+
+        self.clear()  # Clear previous plot
+        self.plot(freq, 20 * np.log10(magnitude))  # Plot magnitude in dB
+
+
+class ColeColeGraph(ParentGraph):
+    def __init__(self):
+        super().__init__()
+          
+        self.setTitle("Cole-cole Graph")
+        self.setLabel('bottom', "Z' [Ohms]")
+        self.setLabel('left', "-Z'' [Ohms]")
+        #self.setAspectLocked(True)  # Lock aspect ratio to 1:1
+
+    def refresh_graph(self, frec, Z_real, Z_imag):
+        """
+        Updates the graph display with the latest computed data.
+        """
+        self.clear()  # Clear previous plot
+        self.plot(Z_real, Z_imag)  # Plot the points
+
+    
+
+#MM
+#this widget will ened to be refactored but 
+#right now I don't want to over engineer things
+#I will make it mor eflexible later on too?
 """
 A Widget displaying three graphs
 """
-
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel
-import pyqtgraph as pg
-
 class GraphsWidget(QWidget):
     def __init__(self):
         super().__init__()
-        self._initialize_ui()
-        
-        self._initialize_ui()
-        
-        
-    def _left_hand_graphs(self):
-        self.big_graph = pg.PlotWidget()
 
-        # Create labels for the graphs
-        self.big_graph_label = QLabel("Big Graph")
-        
-        big_graph_layout = QVBoxLayout()
-        big_graph_layout.addWidget(self.big_graph_label)
-        big_graph_layout.addWidget(self.big_graph)
-        
-        return big_graph_layout
-        
-    def _right_hand_graphs(self):
-        
         # Initialize the graph widgets
-        self.small_graph_1 = pg.PlotWidget()
-        self.small_graph_2 = pg.PlotWidget()
-        
-        # Create labels for the graphs
-        self.small_graph_1_label = QLabel("Small Graph 1")
-        self.small_graph_2_label = QLabel("Small Graph 2")
-        
+        self.big_graph = ColeColeGraph()
+        self.small_graph_1 = BodeGraph()
+        self.small_graph_2 = PhaseGraph()
+
+
         # Layout for the stacked small graphs on the right
         right_graphs_layout = QVBoxLayout()
-        right_graphs_layout.addWidget(self.small_graph_1_label)
         right_graphs_layout.addWidget(self.small_graph_1)
-        right_graphs_layout.addWidget(self.small_graph_2_label)
         right_graphs_layout.addWidget(self.small_graph_2)
-        return right_graphs_layout
-        
-
-    def _initialize_ui(self):
-        left_graphs_layout=self._left_hand_graphs()
-        right_graphs_layout=self._right_hand_graphs()
 
         # Layout for all graphs
         graphs_layout = QHBoxLayout()
-        
+        big_graph_layout = QVBoxLayout()
+        big_graph_layout.addWidget(self.big_graph)
 
-        graphs_layout.addLayout(left_graphs_layout)
+        graphs_layout.addLayout(big_graph_layout)
         graphs_layout.addLayout(right_graphs_layout)
 
         # Set the main layout
         self.setLayout(graphs_layout)
+        
         
 if __name__ == "__main__":
 
